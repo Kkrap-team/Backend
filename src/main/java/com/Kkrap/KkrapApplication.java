bb9bf314b212ec3f44345f37863f8e3d00c26606
JaeYoonJung08 <whitecy01@naver.com>
2024-08-10 18:03:53 +0900

    feat: postgreSQL과 spring boot 연동

---
+++ src/main/java/com/Kkrap/KkrapApplication.java
@@ -0,0 +1,13 @@
+package com.Kkrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkrapApplication.class, args);
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/com/Kkrap/KkrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kkrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
