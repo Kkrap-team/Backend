83a0c7fac56c7a5b305d7fddb524ca6b6f2656b0
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-04-06 17:32:34 +0900

    feat: 테스트를 위한 user 가짜 객체 구성

---
+++ src/test/java/com/Kkrap/Fixture/Users.java
@@ -0,0 +1,11 @@
+package com.Kkrap.Fixture;
+
+public class Users {
+    private Long id;
+    private String name;
+
+    public Users(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
