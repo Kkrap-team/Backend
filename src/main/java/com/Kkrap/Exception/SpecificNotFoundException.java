20c8b893fa59ac7effbef75a0bd0e870c8c31e9f
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-04-06 16:38:03 +0900

    fix: 공통 NotFoundException 클래스 구성

--- src/main/java/com/Kkrap/Exception/SpecificNotFoundException.java
+++ src/main/java/com/Kkrap/Exception/SpecificNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND) // 404 Not Found 상태 코드 반환
-public class FoldersNotFoundException extends RuntimeException {
-    public FoldersNotFoundException(String message) {
+public class SpecificNotFoundException extends RuntimeException {
+    public SpecificNotFoundException(String message) {
         super(message);
     }
 }
