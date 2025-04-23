cf76ff8ed5bb8b11cbcda2dbfadd55163c19ec46
whitecy01 <whitecy01@naver.com>
2025-04-23 16:18:44 +0900

    refactor : DTO Schema 어노테이션 삭제, ErrorCode enum 값 변경

--- src/main/java/com/Kkrap/RequestDTO/LinksCreateRequest.java
+++ src/main/java/com/Kkrap/RequestDTO/LinksCreateRequest.java
@@ -1,6 +1,5 @@
 package com.Kkrap.RequestDTO;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,13 +11,10 @@ import lombok.Setter;
 @NoArgsConstructor
 public class LinksCreateRequest {
 
-    @Schema(description = "저장할 링크 URL", example = "https://~~~~", required = true)
     private String linkUrl;
 
-    @Schema(description = "저장할 폴더 Id", example = "3", required = true)
     private Long foldersId;
 
-    @Schema(description = "모든 링크 폴더 Id", example = "4", required = true)
     private Long defaultFoldersId;
 
 }
