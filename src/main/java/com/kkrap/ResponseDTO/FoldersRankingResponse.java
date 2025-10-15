3fdca08e56c3cb408e997eeb8654562f902892cd
whitecy01 <whitecy01@naver.com>
2025-10-16 01:39:36 +0900

    chore : FoldersRankingResponse 오류 수정

--- src/main/java/com/kkrap/ResponseDTO/FoldersRankingResponse.java
+++ src/main/java/com/kkrap/ResponseDTO/FoldersRankingResponse.java
@@ -2,14 +2,12 @@ package com.kkrap.ResponseDTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class FoldersRankingResponse {
     private List<OneFoldersAllLinksResponse> topViewCount;
